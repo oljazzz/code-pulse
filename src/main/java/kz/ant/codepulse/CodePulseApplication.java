031c65b895d9df3d6beee9824c910c368441b0d7
User <oljazzz362@gmail.com>
2023-11-24 00:01:49 +0600

    initial commit

---
+++ src/main/java/kz/ant/codepulse/CodePulseApplication.java
@@ -0,0 +1,13 @@
+package kz.ant.codepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/ant/codepulse/CodePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.ant.codepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
